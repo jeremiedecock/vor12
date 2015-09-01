1b91415285aef7b852c2cc5a1185c9da4b0d476e
Jérémie Decock <jd.jdhp@gmail.com>
2015-09-01 18:40:13 +0200

    Fix a bug.

--- vor12/actuator/dynamixel_ax12.py
+++ vor12/actuator/dynamixel_ax12.py
@@ -91,12 +91,12 @@ class DynamixelAX12(object):
         pos_z = self.connection.get_present_position(self.dynamixel_z_axis_id)
 
         # The x_axis controls up/down movements
-        new_pos_x = pos_x + control_vect[1]
+        new_pos_x = pos_x + int(control_vect[1])
 
         # The z_axis controls left/right movements
         # Warning: movements are inverted on the z axis
         #          (negative commands move the frame to the right)
-        new_pos_z = pos_z - control_vect[0]
+        new_pos_z = pos_z - int(control_vect[0])
 
         #try:
         self.connection.goto(self.dynamixel_x_axis_id, new_pos_x, speed=512)
